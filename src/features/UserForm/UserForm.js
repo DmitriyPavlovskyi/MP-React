46a2b528fb0c88f9b703a89e5d9e34aeb00d8893
Dmytro Pavlovskyi1 <pavlovskyidmitriy@gmail.com>
2017-11-17 18:03:23 +0200

    Selecting for values Form added

--- src/features/UserForm/UserForm.js
+++ src/features/UserForm/UserForm.js
@@ -1,41 +1,93 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Field, reduxForm } from 'redux-form';
-import {errorStyles} from '../shared/formsValidationStyles';
-import validate from './validate';
-import asyncValidate from './asyncValidate';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm, formValueSelector } from 'redux-form';
 
-//-----------Не совсем понятна эта функция-----------
-const renderField = ({input, label, type, meta: { touched, error }}) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} placeholder={label} type={type} />
-      {touched && error && <span style={errorStyles}>{error}</span>}
-    </div>
-  </div>
-);
-
-const AsyncValidationForm = props => {
-  const { handleSubmit, pristine, reset, submitting } = props;
+function handleSubmitData(values) {
+  console.log(values);
+}
+let SelectingFormValuesForm = props => {
+  const {
+    favoriteColorValue,
+    fullName,
+    handleSubmit,
+    hasEmailValue,
+    pristine,
+    reset,
+    submitting
+  } = props;
 
   return (
-    <form onSubmit={handleSubmit(handleSubmit)}>
-      <Field
-        name="username"
-        type="text"
-        component={renderField}
-        label="Username"
-      />
-      <Field
-        name="password"
-        type="password"
-        component={renderField}
-        label="Password"
-      />
+    <form onSubmit={handleSubmit(handleSubmitData)}>
       <div>
-        <button type="submit" disabled={submitting}>
-          Log In
+        <label>First Name</label>
+        <div>
+          <Field
+            name="firstName"
+            component="input"
+            type="text"
+            placeholder="First Name"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Last Name</label>
+        <div>
+          <Field
+            name="lastName"
+            component="input"
+            type="text"
+            placeholder="Last Name"
+          />
+        </div>
+      </div>
+      <div>
+        <label htmlFor="hasEmail">Has Email?</label>
+        <div>
+          <Field
+            name="hasEmail"
+            id="hasEmail"
+            component="input"
+            type="checkbox"
+          />
+        </div>
+      </div>
+      {hasEmailValue && (
+        <div>
+          <label>Email</label>
+          <div>
+            <Field
+              name="email"
+              component="input"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+        </div>
+      )}
+      <div>
+        <label>Favorite Color</label>
+        <div>
+          <Field name="favoriteColor" component="select">
+            <option />
+            <option value="#ff0000">Red</option>
+            <option value="#00ff00">Green</option>
+            <option value="#0000ff">Blue</option>
+          </Field>
+        </div>
+      </div>
+      {favoriteColorValue && (
+        <div
+          style={{
+            height: 80,
+            width: 200,
+            margin: '10px auto',
+            backgroundColor: favoriteColorValue
+          }}
+        />
+      )}
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit {fullName}
         </button>
         <button type="button" disabled={pristine || submitting} onClick={reset}>
           Clear Values
@@ -45,17 +97,26 @@ const AsyncValidationForm = props => {
   );
 };
 
-AsyncValidationForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  pristine: PropTypes.bool.isRequired,
-  submitting: PropTypes.bool.isRequired,
-  reset: PropTypes.func.isRequired,
-  error: PropTypes.string
-};
+// The order of the decoration does not matter.
 
-export default reduxForm({
-  form: 'asyncValidation',
-  validate,
-  asyncValidate,
-  asyncBlurFields: ['username']
-})(AsyncValidationForm);
+// Decorate with redux-form
+SelectingFormValuesForm = reduxForm({
+  form: 'selectingFormValues' // a unique identifier for this form
+})(SelectingFormValuesForm);
+
+// Decorate with connect to read form values
+const selector = formValueSelector('selectingFormValues') // <-- same as form name
+SelectingFormValuesForm = connect(state => {
+  // can select values individually
+  const hasEmailValue = selector(state, 'hasEmail');
+  const favoriteColorValue = selector(state, 'favoriteColor');
+  // or together as a group
+  const { firstName, lastName } = selector(state, 'firstName', 'lastName');
+  return {
+    hasEmailValue,
+    favoriteColorValue,
+    fullName: `${firstName || ''} ${lastName || ''}`
+  };
+})(SelectingFormValuesForm);
+
+export default SelectingFormValuesForm;
